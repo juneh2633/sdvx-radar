6c37547c2b6153592d9cf6f379d01f1ce3ec7a2a
juneh2633 <juneh2633@gmail.com>
2024-04-28 09:42:56 +0900

    feat: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { AuthService } from './auth/auth.service';
 import { SongService } from './song/song.service';
 import { SongController } from './song/song.controller';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './common/config/typeorm.config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController, SongController],
   providers: [AppService, AuthService, SongService],
 })
---
+++ src/common/config/typeorm.config.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USER'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_DATABASE'),
+      entities: [],
+    };
+  }
+}
