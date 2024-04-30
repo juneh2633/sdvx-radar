637210749b97307b4f87ac648db1128117e5e064
juneh2633 <juneh2633@gmail.com>
2024-04-30 20:41:54 +0900

    feat: song controller

--- src/song/song.controller.ts
+++ src/song/song.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, Response } from '@nestjs/common';
+import { SongService } from './song.service';
+import { SelectSongPagerbleDto } from './dto/select-song-pagerble.dto';
+import { SelectSongByIdDto } from './dto/select-song-id.dto';
 
 @Controller('song')
-export class SongController {}
+export class SongController {
+  constructor(private readonly songService: SongService) {}
+
+  @Get('/all')
+  public async findSongs(@Query() pagerbleDto: SelectSongPagerbleDto) {
+    return await this.songService.getSongAll(pagerbleDto);
+  }
+
+  @Get('/:id')
+  public async findSongByid(@Param() songByDto: SelectSongByIdDto) {
+    return await this.songService.getSongById(songByDto);
+  }
+}
