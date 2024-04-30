82d97c23d87f922ff5825dd100078980d0b4e44a
juneh2633 <juneh2633@gmail.com>
2024-04-30 20:40:43 +0900

    feat: song service

--- src/song/song.service.ts
+++ src/song/song.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { SongRepository } from './song.repository';
+import { SelectSongPagerbleDto } from './dto/select-song-pagerble.dto';
+import { SelectSongByIdDto } from './dto/select-song-id.dto';
+import { SongEntity } from './entity/song.entity';
 
 @Injectable()
-export class SongService {}
+export class SongService {
+  constructor(private readonly songRepository: SongRepository) {}
+
+  public async getSongById(selectDto: SelectSongByIdDto): Promise<SongEntity> {
+    return await this.songRepository.selectSongById({
+      id: selectDto.id,
+    });
+  }
+
+  public async getSongAll(
+    selectDto: SelectSongPagerbleDto,
+  ): Promise<SongEntity[]> {
+    return await this.songRepository.selectSongAll({
+      limit: 10,
+      offset: (selectDto.page - 1) * 10,
+    });
+  }
+}
