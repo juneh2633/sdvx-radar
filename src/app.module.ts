e22f8215e8dd6ad8599505cb3a687b6d48f3ac64
juneh2633 <juneh2633@gmail.com>
2024-04-30 20:41:42 +0900

    chore: unneccesary code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
-import { SongService } from './song/song.service';
-import { SongController } from './song/song.controller';
+
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './common/config/typeorm.config';
-import { ScoreService } from './score/score.service';
+
+import { SongModule } from './song/song.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '../.env',
     }),
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
     }),
     AuthModule,
+    SongModule,
   ],
-  controllers: [AppController, SongController],
-  providers: [AppService, AuthService, SongService, ScoreService],
 })
 export class AppModule {}
